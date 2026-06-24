55de1c3c38efbf97cafb32f2fcfee332723dfc6a
Aakrit Neupane <aakritneupane53@gmail.com>
2026-06-24 23:46:32 +0545

    Post resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
