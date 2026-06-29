ac55cbfa94f02d711694bead6cf5156c9d376659
Aakrit Neupane <aakritneupane53@gmail.com>
2026-06-30 00:51:47 +0545

    Register user done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  async register(registerUserDto: RegisterUserDto) {
+    return this.userService.createUser(registerUserDto);
+  }
+}
